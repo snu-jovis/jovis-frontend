367a4ffbaf2b57a8d357160a1ac9e9fa181d5e61
Kyoseung Koo <grammaright@gmail.com>
2024-01-24 15:05:03 +0900

    update app.js and readme.md

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState("");
+
+  const updateValue = (event) => {
+    setQuery(event.target.value);
+  }
+
+  const click = () => {
+    // backend에 SQL 전송 및 결과 받아옴.
+    fetch("http://localhost:8000/query/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ query: query })
+    })
+      .then(response => {
+        if (response.status !== 200) {
+          response.text().then(e => alert("Error: " + e));
+          return
+        }
+        return response.json()
+      })
+      .then(data => setResult(data.result));    // 성공하면 결과를 result에 저장
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        {/* 귀여운 리엑트 로고 */}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        {/* 쿼리 입력하는 곳 */}
+        <span>Query:</span> 
+        <input type="text" onChange={updateValue} />
+        <input type="button" value="Submit" onClick={click} />
+
+        <br /><br />
+        {/* 결과 나오는 곳 */}
+        <span>Result:</span>
+        <p>{result}</p>
       </header>
     </div>
   );
