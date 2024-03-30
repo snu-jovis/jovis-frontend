b08ead97793ec3bfb6a7205695b32a36a9409e62
yoojin-dbs <cyj@dbs.snu.ac.kr>
2024-03-30 16:36:03 +0900

    animation

--- src/components/dp/GraphView.js
+++ src/components/dp/GraphView.js
@@ -3,17 +3,22 @@ import { parseDp, parseOptimalOne } from "./parseDp";
 import { sliderHorizontal } from "d3-simple-slider";
 import * as d3 from "d3";
 import * as d3dag from "https://cdn.skypack.dev/d3-dag@1.0.0-1";
-import { Checkbox, slider } from "@material-tailwind/react";
+import { Checkbox } from "@material-tailwind/react";
 
 import "../../assets/stylesheets/Dp.css";
 
 const GraphView = ({ width, height, data }) => {
   const dagSvg = useRef(null);
+  const svgWidth = width;
+  const svgHeight = height - 80;
+
   const sliderRef = useRef(null);
   const sliderWidth = 300;
   const sliderHeight = 50;
   const sliderMargin = 10;
-  const sliderSize = 80;
+
+  const [moving, setMoving] = useState(false);
+  const [currLevel, setCurrLevel] = useState(1);
 
   const target = data.optimizer.dp[data.optimizer.dp.length - 1];
   const totalCost = target.cheapest_total_paths.total_cost;
@@ -24,69 +29,78 @@ const GraphView = ({ width, height, data }) => {
     setShowOptimalOne((prev) => !prev);
   };
 
-  function drawSlider({ sliderSvg }) {
-    d3.select(sliderSvg.current).selectAll("*").remove(); //clear
+  function mapName(name) {
+    switch (name) {
+      case "SeqScan":
+        return "Seq\nScan";
+      case "HashJoin":
+        return "Hash\nJoin";
+      case "MergeJoin":
+        return "Merge\nJoin";
+      case "NestLoop":
+        return "Nested\nLoop";
+      case "IdxScan":
+        return "Index\nScan";
+      default:
+        return name;
+    }
+  }
 
-    var margin = { top: 50, right: 50, bottom: 0, left: 50 },
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
-    var moving = false;
-    var currentValue = 0;
+  function drawSlider({ data }) {
+    const sliderSvg = d3.select(sliderRef.current);
+    sliderSvg.selectAll("*").remove(); //clear
+
     var interval;
-    const tickValue = d3.range(0, 11);
 
-    const svg = d3.select(sliderSvg.current);
+    const maxLevel = Math.ceil((data[data.length - 1].level + 1) / 2);
+    const tickValue = d3.range(1, maxLevel + 1);
 
-    svg
+    var playButton = d3.select("#play-button");
+
+    sliderSvg
       .append("svg")
       .attr("width", sliderWidth)
       .attr("height", sliderHeight)
       .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    // play/pause button
-    var playButton = d3.select("#play-button");
-
-    // create slider
     var slider = sliderHorizontal()
-      .min(0)
-      .max(10)
+      .min(1)
+      .max(maxLevel)
       .step(1)
-      .width(sliderWidth)
+      .width(sliderWidth - 2 * sliderMargin)
       .tickValues(tickValue)
-      .default(0)
+      .default(1)
       .on("onchange", (val) => {
-        currentValue = val;
-        console.log("Slider value: ", val);
+        setCurrLevel(val);
       });
 
-    var sliderRange = svg
+    var sliderRange = sliderSvg
       .append("g")
-      .attr(
-        "transform",
-        `translate${sliderSize + sliderMargin * 2}, ${sliderMargin / 2})`
-      );
-
+      .attr("transform", `translate(${sliderMargin}, ${sliderMargin / 2})`);
     sliderRange.call(slider);
 
     playButton.on("click", function () {
       var button = d3.select(this);
+
       if (moving) {
         clearInterval(interval);
-        moving = false;
+        setMoving(false);
         button.text("Play");
       } else {
-        moving = true;
+        setMoving(true);
         button.text("Pause");
         interval = setInterval(function () {
-          currentValue = currentValue < 10 ? currentValue + 1 : 0;
-          slider.value(currentValue);
-          if (currentValue === 10) {
+          currLevel < maxLevel ? setCurrLevel(currLevel + 1) : setCurrLevel(1);
+
+          slider.value(currLevel);
+
+          if (currLevel === maxLevel) {
             clearInterval(interval);
-            moving = false;
+            setMoving(false);
             playButton.text("Play");
           }
-        }, 1000);
+        }, 500);
       }
     });
   }
@@ -97,24 +111,20 @@ const GraphView = ({ width, height, data }) => {
     // data graph 형태로 변경
     const graph = d3dag.graphStratify()(data);
 
-    /* coumput layout */
-    const nodeRadius = 25;
-    const nodeSize = [nodeRadius * 2, nodeRadius * 2];
+    /* coumpute layout */
+    const nodeSize = 50;
     const dagDepth = data[data.length - 1].level;
 
     const layout = d3dag
       .sugiyama()
-      .nodeSize((node) => {
-        if (node.data.id.includes(" - ")) {
-          return [150, 150];
-        } else {
-          return [50, 50];
-        }
-      })
-      .gap([20, 20]);
-    // .tweaks([shape]);
-
+      .nodeSize([nodeSize, nodeSize])
+      .gap([nodeSize / 3, nodeSize / 3]);
     const { width: dagWidth, height: dagHeight } = layout(graph);
+
+    const scale =
+      svgWidth / dagWidth < svgHeight / (dagHeight + 2 * margin.y)
+        ? svgWidth / dagWidth
+        : svgHeight / (dagHeight + 2 * margin.y);
 
     const svg = d3
       .select(graphSvg.current)
@@ -122,7 +132,7 @@ const GraphView = ({ width, height, data }) => {
       .attr("width", dagWidth)
       .attr("height", dagHeight)
       .append("g") // 그룹으로 묶어서
-      .attr("transform", `scale(${width / dagWidth}, ${width / dagWidth})`)
+      .attr("transform", `scale(${scale}, ${scale})`)
       .call(
         d3.zoom().on("zoom", (event) => {
           svg.attr("transform", event.transform);
@@ -131,7 +141,6 @@ const GraphView = ({ width, height, data }) => {
       .append("g");
 
     // create links
-    const line = d3.line().curve(d3.curveMonotoneY);
     const links = svg
       .append("g")
       .selectAll("path")
@@ -139,15 +148,9 @@ const GraphView = ({ width, height, data }) => {
       .enter()
       .append("path")
       .attr("d", (d) =>
-        line([
-          [
-            d.source.x,
-            d.source.data.level * (dagHeight / dagDepth) + nodeRadius / 2,
-          ],
-          [
-            d.target.x,
-            d.target.data.level * (dagHeight / dagDepth) - nodeRadius / 2,
-          ],
+        d3.line()([
+          [d.source.x, d.source.data.level * (dagHeight / dagDepth)],
+          [d.target.x, d.target.data.level * (dagHeight / dagDepth)],
         ])
       )
       .attr("transform", `translate(0, ${margin.y})`)
@@ -167,31 +170,6 @@ const GraphView = ({ width, height, data }) => {
           d.data.level * (dagHeight / dagDepth) + margin.y
         })`;
       });
-
-    // // create arrows
-    // const arrowSize = (nodeRadius * nodeRadius) / 20.0;
-    // const arrowLen = Math.sqrt((4 * arrowSize) / Math.sqrt(3));
-    // const arrow = d3.symbol().type(d3.symbolTriangle).size(arrowSize);
-
-    // svg
-    //   .append("g")
-    //   .selectAll("path")
-    //   .data(graph.links())
-    //   .enter()
-    //   .append("path")
-    //   .attr("d", arrow)
-    //   .attr("transform", ({ points }) => {
-    //     const [[sx, sy], [ex, ey]] = points.slice(-2);
-    //     const dx = sx - ex;
-    //     const dy = sy - ey;
-    //     // This is the angle of the last line segment
-    //     const angle = (Math.atan2(-dy, -dx) * 180) / Math.PI + 90;
-    //     return `translate(${ex}, ${ey}) rotate(${angle})`;
-    //   })
-    //   .attr("fill", ({ target }) => colorMap[target.data.id])
-    //   .attr("stroke", "white")
-    //   .attr("stroke-width", 1.5)
-    //   .attr("stroke-dasharray", `${arrowLen},${arrowLen}`);
 
     const colorMap = new Map();
     const nodesArray = Array.from(graph.nodes());
@@ -216,15 +194,15 @@ const GraphView = ({ width, height, data }) => {
         node
           .append("rect")
           .attr("id", (d) => d.data.id.replace(/\s/g, ""))
-          .attr("width", d.data.id.length * 6 + 20)
+          .attr("width", 50)
           .attr("height", 50)
-          .attr("x", -d.data.id.length * 3 - 10)
-          .attr("y", -15)
+          .attr("x", -nodeSize / 2)
+          .attr("y", -nodeSize / 2)
           .attr("fill", colorMap.get(parts[1]));
       } else {
         node
           .append("circle")
-          .attr("r", nodeRadius)
+          .attr("r", nodeSize / 2)
           .attr("fill", colorMap.get(nodeTypes[0]));
       }
     });
@@ -232,10 +210,22 @@ const GraphView = ({ width, height, data }) => {
     // node type
     nodes
       .append("text")
-      .text((d) => d.data.id.split(" - ").pop())
       .attr("text-anchor", "middle")
       .attr("alignment-baseline", "middle")
-      .attr("class", "dp-node-text");
+      .attr("class", "dp-node-text")
+      .each(function (d) {
+        const lines = mapName(d.data.id.split(" - ").pop()).split("\n");
+        if (lines.length === 1) d3.select(this).text(lines[0]);
+        else {
+          for (let i = 0; i < lines.length; i++) {
+            d3.select(this)
+              .append("tspan")
+              .text(lines[i])
+              .attr("x", 0)
+              .attr("dy", i ? "1.2em" : "-0.2em");
+          }
+        }
+      });
 
     const tooltip = d3
       .select("body")
@@ -257,10 +247,6 @@ const GraphView = ({ width, height, data }) => {
       .on("mouseout", function () {
         tooltip.style("visibility", "hidden");
       });
-
-    /* ANIMATION */
-    nodes.style("opacity", "0");
-    links.style("opacity", "0");
 
     function animate(level) {
       if (level > dagDepth) return;
@@ -285,7 +271,7 @@ const GraphView = ({ width, height, data }) => {
           return d.data.level <= level + 1 && d.data.level > level - 1;
         })
         .transition()
-        .duration(10)
+        .duration(500)
         .style("opacity", 1);
 
       links
@@ -295,7 +281,7 @@ const GraphView = ({ width, height, data }) => {
           );
         })
         .transition()
-        .duration(10)
+        .duration(500)
         .style("opacity", 1)
         .end()
         .then(() => {
@@ -308,7 +294,7 @@ const GraphView = ({ width, height, data }) => {
               );
             })
             .transition()
-            .duration(10)
+            .duration(500)
             .style("opacity", 0);
 
           links
@@ -326,7 +312,7 @@ const GraphView = ({ width, height, data }) => {
               );
             })
             .transition()
-            .duration(10)
+            .duration(500)
             .style("opacity", 0)
             .end()
             .then(() => {
@@ -335,8 +321,11 @@ const GraphView = ({ width, height, data }) => {
         });
     }
 
-    // Start the animation with level 0
-    animate(0);
+    if (moving) {
+      nodes.style("opacity", "0");
+      links.style("opacity", "0");
+      animate(0);
+    }
   }
 
   useEffect(() => {
@@ -348,44 +337,59 @@ const GraphView = ({ width, height, data }) => {
         graphSvg: dagSvg,
         data: optimalData,
       });
-    else
+    else {
       drawGraph({
         graphSvg: dagSvg,
         data: dpData,
       });
-  }, [data, width, height, showOptimalOne]);
+      drawSlider({
+        data: dpData,
+      });
+    }
+  }, [data, moving, svgWidth, svgHeight, showOptimalOne]);
 
   useEffect(() => {
+    const dpData = parseDp(data);
+
     drawSlider({
-      sliderSvg: sliderRef,
+      data: dpData,
     });
   }, [sliderRef]);
 
   return (
-    <div>
-      <p className="dp-text">Total Cost: {totalCost}</p>
-      <div className="flex justify-center flex-container">
-        <button className="dp-text" id="play-button">
-          Play
-        </button>
-        <svg
-          className="slider"
-          ref={sliderRef}
-          width={sliderWidth}
-          height={sliderHeight}
-        />
+    <>
+      <div className="flex justify-center items-center gap-6 pb-4">
+        <div className="stats shadow">
+          <div className="flex m-2 gap-2">
+            <div className="dp-total-cost">Total Cost</div>
+            <div className="dp-cost-value">{totalCost}</div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <button className="dp-play-text" id="play-button">
+            Play
+          </button>
+          <svg
+            className="slider"
+            ref={sliderRef}
+            width={sliderWidth + sliderMargin * 2}
+            height={sliderHeight}
+          />
+        </div>
       </div>
-      <svg ref={dagSvg} width={width} height={height + 2 * margin.y}></svg>
-      <div className="checkbox-container">
-        <Checkbox
-          color="blue"
-          className="h-4 w-4 rounded-full border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
-          checked={showOptimalOne}
-          label={<p className="text">Show Optimized One</p>}
-          onClick={handleCheckboxChange}
-        />
+      <div>
+        <svg ref={dagSvg} width={svgWidth} height={svgHeight + 4 * margin.y} />
+        <div className="checkbox-container">
+          <Checkbox
+            color="blue"
+            className="h-4 w-4 rounded-full border-gray-900/20 bg-gray-900/10 transition-all hover:scale-105 hover:before:opacity-0"
+            checked={showOptimalOne}
+            label={<p className="text">Show Optimized One</p>}
+            onChange={handleCheckboxChange}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
